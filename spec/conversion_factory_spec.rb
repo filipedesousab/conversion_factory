ff595902c2c2f333ebb87954594a82e275b98181
Filipe Botelho <filipedesousab@gmail.com>
2024-01-03 23:11:07 -0300

    Remove wrong test

--- spec/conversion_factory_spec.rb
+++ spec/conversion_factory_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ConversionFactory do
   it 'has a version number' do
     expect(ConversionFactory::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
