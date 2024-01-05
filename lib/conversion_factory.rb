7bd5910d37e09f59522d390793c0d74edca0d7ed
Filipe Botelho <filipedesousab@gmail.com>
2024-01-05 19:23:01 -0300

    Add configuration with output_path

--- lib/conversion_factory.rb
+++ lib/conversion_factory.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'conversion_factory/build'
+require_relative 'conversion_factory/configuration'
 require_relative 'conversion_factory/entities'
 require_relative 'conversion_factory/errors'
 require_relative 'conversion_factory/version'
 
 module ConversionFactory
   module_function
+
+  def configuration
+    @configuration ||= Configuration.new
+  end
+
+  def configure
+    yield(configuration)
+  end
 
   def build(**params)
     Build.new(**params)
