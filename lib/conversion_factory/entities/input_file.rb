bda092521d8ac3e500bd895295b7abea4cecdbe2
Filipe Botelho <filipedesousab@gmail.com>
2024-02-07 20:42:40 -0300

    Set input file name as default output file name

--- lib/conversion_factory/entities/input_file.rb
+++ lib/conversion_factory/entities/input_file.rb
@@ -6,14 +6,13 @@ require 'tmpdir'
 module ConversionFactory
   module Entities
     class InputFile
-      attr_reader   :file, :content_type, :output_path
-      attr_accessor :output_filename
+      attr_reader :file, :content_type, :output_path, :output_filename
 
       def initialize(file: nil, content_type: nil, output_path: nil, output_filename: nil)
         self.file = file if file
         self.content_type = content_type if content_type
         self.output_path = output_path if output_path
-        @output_filename = output_filename
+        self.output_filename = output_filename
       end
 
       def file=(file)
@@ -21,6 +20,7 @@ module ConversionFactory
 
         @file = Pathname.new(file)
         set_content_type
+        set_output_filename if output_filename.to_s.empty?
         @file # rubocop:disable Lint/Void
       end
 
@@ -32,10 +32,18 @@ module ConversionFactory
         @output_path = Pathname.new(output_path)
       end
 
+      def output_filename=(output_filename = nil)
+        @output_filename = output_filename || set_output_filename
+      end
+
       private
 
       def set_content_type
         @content_type ||= FileMagic.mime.file(file.to_s, true) if file
+      end
+
+      def set_output_filename
+        @output_filename = file&.basename&.to_s
       end
     end
   end
